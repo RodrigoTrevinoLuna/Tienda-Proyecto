e6d356d52efe4c428e7d7ca442e354e71d85360a
Ivan <narvaezivan411@gmail.com>
2021-11-21 16:10:13 -0600

    ivan

--- cajero.php
+++ cajero.php
@@ -271,7 +271,7 @@
                                                 </section>
                                                 <footer class="modal-footer">
                                                     <!--<button>Imprimir Ticket</button>-->
-                                                    <button>Terminar Venta </button>
+                                                    <button>Terminar Venta</button>
                                                 </footer>
                                                 </form>
                                             </div>
